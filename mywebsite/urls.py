54fb84a9a4e1a9153373e024e41ffc82e7ffe8a3
Silviya <90373733+slvya@users.noreply.github.com>
2021-10-19 19:30:08 +0700

    Add files via upload

---
+++ mywebsite/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url , include
+from django.contrib import admin
+ 
+from . import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url('', include('tips.urls'))
+     
+     
+]
